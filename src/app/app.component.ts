ed90313066ecdb7b1958d2414548e219334b71a7
deathm0r4 <sthinkholelapeno@gmail.com>
2025-01-27 20:40:01 -0600

    Proyecto Red Social

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AsideComponent } from './aside/aside.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, AsideComponent, FooterComponent, HeaderComponent, ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,34 @@
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-profile',
+  imports: [FormsModule],
+  templateUrl: './profile.component.html',
+  styleUrl: './profile.component.css'
+})
+export class ProfileComponent {
+  //Atributos
+  user= {
+  firstName: "Luis",
+  lastName: "Mora",
+  email: "luisedumr@hotmail.com",
+  username: "lu.mora9916",
+  imageUrl: "/chico.jpg",
+  };
+
+  //Métodos
+  //Cambio de imagen
+  onImageChange(event: Event){
+    const file = (event.target as HTMLInputElement).files?.[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload=()=>{
+        this.user.imageUrl=reader.result as string;
+      };
+
+      reader.readAsDataURL(file);
+    }//fin del if
+  }//fin del método
+  
+}
